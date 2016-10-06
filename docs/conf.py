3dc4151afb3aebc68996916cb2b5d158d065b25c
Hiroshige Negishi <hirosige1@gmail.com>
2016-10-06 18:48:57 +0700

    add new page

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Engine Yard Deploy Guide'
-copyright = u'2014, Read the Docs'
+copyright = u'2016, Hiroshige Negishi ARMS Thailand'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
